e6c8ca1d743c43c4e503ce5969a00dded8d79a4e
joaovflorisvaldo <joaovflorisvaldo@gmail.com>
2024-04-11 00:29:47 -0300

    Adicionano aquivos iniciais OSauto

---
+++ OSauto/src/main/java/com/mycompany/osauto/main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.osauto;
+
+
+/**
+ *
+ * @author joaov
+ */
+public class main {
+    public static void main(String[] args) {
+        new FramePrincipal().setVisible(true);
+    }
+}
